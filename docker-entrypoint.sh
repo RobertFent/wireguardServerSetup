cca0d1e9b532c7735610e5462c703b517086a269
RobertFent <robert.fent@outlook.de>
2020-06-06 07:33:28 +0000

    removed some comments from entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,7 @@
 
 /init.sh
 
-#publickeyclient = $( cat /keys/publickeyclient )
-
-#echo $publickeyclient
-# wg set wg0 peer $publickkeyclient endpoint 46.83.86.42:51820 allowed-ips 
-
 wg-quick up wg0
 
+# to make container run infinit
 tail -f /dev/null
